ce817d3db2dec380d6078dea53ad433a1aacf4a0
burningfiremoon <charleszhang4@gmail.com>
2024-03-29 10:57:43 -0700

    initial commit

---
+++ IPQ.h
@@ -0,0 +1,27 @@
+#ifndef IPQ_H
+#define IPQ_H
+
+#include <iostream>
+using namespace std;
+
+template <typename Object>
+class IPQ
+{
+    public:
+    
+    // constructor
+
+    // Deconstructor
+
+    private:
+
+}
+
+
+
+
+
+
+
+
+#endif
